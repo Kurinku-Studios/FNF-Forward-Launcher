626d8c0bd93161e8c631296078e47707c44a0900
mosemister <nekronmoseley@gmail.com>
2021-06-14 22:21:17 +0100

    Fixed POM

--- src/main/java/ForwardLauncher.java
+++ src/main/java/ForwardLauncher.java
@@ -1,2 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 public class ForwardLauncher extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        ForwardLauncher.launch(args);
+    }
 }
